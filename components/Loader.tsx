6bff1230d6713c6006b70c6de57cf33d80edac3a
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-19 11:09:54 +0530

    *

---
+++ components/Loader.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+const Loader = () => (
+  <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
+   
+    <p className='text-sm font-bold text-primary-grey-300'>Configuring as ADV</p>
+  </div>
+);
+
+export default Loader;
